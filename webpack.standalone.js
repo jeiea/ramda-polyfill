49436a7648c14266f7c96d143ae59991caefa7c0
jeiea <solum5013@gmail.com>
2017-01-10 12:51:30 +0900

    Works well as a library.

--- webpack.standalone.js
+++ webpack.standalone.js
@@ -8,18 +8,18 @@ module.exports = [{
   },
   output: {
     path: __dirname + '/dist',
-    filename: '[name].js',
-    library: 'ramda-polyfill',
-    libraryTarget: 'commonjs-module'
+    filename: '[name].min.js',
   },
   resolve: {
     modules: ['.', 'node_modules']
   },
-  externals: {
-    'ramda': 'ramda'
-  },
   module: {
     loaders: [{
+      enforce: 'pre',
+      test: /\.js$/,
+      loader: 'eslint-loader',
+      include: path.resolve(__dirname, './index.js'),
+    }, {
       test: /\.js$/,
       loader: 'babel-loader',
       include: path.resolve(__dirname, './index.js'),
@@ -27,5 +27,19 @@ module.exports = [{
         presets: ['es2015']
       }
     }]
-  }
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      cacheFolder: path.resolve(__dirname, 'build/'),
+      debug: true,
+      minimize: true,
+      sourceMap: true,
+      output: {
+        comments: false
+      },
+      compressor: {
+        warnings: false
+      }
+    }),
+  ]
 }];
